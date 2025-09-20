9245b1878c3c42a1fb547cc24669dc1ed4be736c
Jeremiah <jeremiahjnairn@gmail.com>
2025-09-20 14:51:40 -0400

    0.0.0 adding the files and preparing to plan them out

---
+++ main.py
@@ -0,0 +1,16 @@
+"""
+File: main.py
+Author: Jeremiah Nairn
+
+Description: Foundation for the program. 
+"""
+
+#other functions go here if you have them
+
+#main function (where foundational code should go)
+def main():
+    print("code goes here")
+
+#execute main
+if __name__ == '__main__':
+    main()
